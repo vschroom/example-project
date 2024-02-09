81c88e2bc0d173591314d876a774fc4cf5b39334
vchs <vchs1710@yandex.ru>
2024-02-09 18:22:45 +0300

    add lock for transfer balance

--- src/main/java/com/chernov/exampleproject/service/impl/TransferServiceImpl.java
+++ src/main/java/com/chernov/exampleproject/service/impl/TransferServiceImpl.java
@@ -8,12 +8,14 @@ import com.chernov.exampleproject.service.TransferService;
 import com.chernov.exampleproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -22,11 +24,25 @@ public class TransferServiceImpl implements TransferService {
 
     private final AccountRepository accountRepository;
     private final UserService userService;
+    private final RedissonClient redissonClient;
 
     @Override
-    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public void transferBalanceFromCurrentUserAccount(TransferRequest request) {
+        RLock transferLock = redissonClient.getLock("transfer");
+        try {
+            transferLock.lock();
+            log.info("Transfer lock");
+            transferBalance(request);
+        } catch (Exception e) {
+            log.error("Error while lock transfer", e);
+        } finally {
+            transferLock.unlock();
+            log.info("Transfer unlock");
+        }
+    }
 
+    @Transactional
+    public void transferBalance(TransferRequest request) {
         Long userIdFrom = userService.getCurrentUser().getId();
         Account accountFrom = findByUser(userIdFrom);
         Account accountTo = findByUser(request.getUserIdTo());
@@ -41,6 +57,8 @@ public class TransferServiceImpl implements TransferService {
         BigDecimal addedBalanceTo = balanceTo.add(sumValue);
         accountFrom.setBalance(subtractedBalanceFrom);
         accountTo.setBalance(addedBalanceTo);
+        accountRepository.saveAllAndFlush(List.of(accountFrom, accountTo));
+
         log.info("User with id {} transfer {} balance to user with id {} at {} ",
                 userIdFrom, sumValue, request.getUserIdTo(), LocalDateTime.now());
     }
--- src/test/java/com/chernov/exampleproject/TransferServiceUnitTest.java
+++ src/test/java/com/chernov/exampleproject/TransferServiceUnitTest.java
@@ -9,23 +9,25 @@ import com.chernov.exampleproject.service.UserService;
 import com.chernov.exampleproject.service.impl.TransferServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import org.mockito.verification.VerificationMode;
+import org.redisson.RedissonLock;
+import org.redisson.api.RedissonClient;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import static java.util.Optional.of;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 public class TransferServiceUnitTest {
 
     private final UserService userService = Mockito.mock(UserService.class);
     private final AccountRepository accountRepository = Mockito.mock(AccountRepository.class);
+    private final RedissonClient redissonClient = Mockito.mock(RedissonClient.class);
+    private final RedissonLock redissonLock = Mockito.mock(RedissonLock.class);
 
-    private final TransferService transferService = new TransferServiceImpl(accountRepository, userService);
+    private final TransferService transferService = new TransferServiceImpl(accountRepository, userService, redissonClient);
 
     @Test
     void shouldTransferBalance() {
@@ -33,16 +35,22 @@ public class TransferServiceUnitTest {
         User user = buildUser();
         Account accountFrom = buildAccount(1L);
         Account accountTo = buildAccount(2L);
+        List<Account> accounts = List.of(accountFrom, accountTo);
+
+        when(redissonClient.getLock(anyString())).thenReturn(redissonLock);
         when(userService.getCurrentUser()).thenReturn(user);
         when(accountRepository.findByUserId(1L)).thenReturn(of(accountFrom));
         when(accountRepository.findByUserId(2L)).thenReturn(of(accountTo));
+        when(accountRepository.saveAllAndFlush(anyList())).thenReturn(accounts);
 
         transferService.transferBalanceFromCurrentUserAccount(transferRequest);
 
+        verify(redissonClient).getLock("transfer");
         verify(userService).getCurrentUser();
         verify(accountRepository).findByUserId(1L);
         verify(accountRepository).findByUserId(2L);
-        verifyNoMoreInteractions(userService, accountRepository);
+        verify(accountRepository).saveAllAndFlush(accounts);
+        verifyNoMoreInteractions(redissonClient, userService, accountRepository);
 
         Assertions.assertThat(accountFrom).isNotNull();
         Assertions.assertThat(accountFrom.getBalance()).isLessThan(accountTo.getBalance());
